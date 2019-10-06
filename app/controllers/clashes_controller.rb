3c0fe29d9a6fb1508ee312f145471525191e082a
Alexander.Iljin <piroxiljin@gmail.com>
2019-10-06 18:12:55 +0700

    Iterate clash table through attack upgrades and armor upgrades

--- app/controllers/clashes_controller.rb
+++ app/controllers/clashes_controller.rb
@@ -3,12 +3,14 @@ class ClashesController < ApplicationController
     render locals: {
         attackers: Unit.all.reject{|u| u.attacks.blank?},
         defenders: Unit.all,
+        attack_upgrade_levels: [0, 1, 2, 3],
+        armor_upgrade_levels: [0, 1, 2, 3],
         operation: method(:damage)
     }
   end
 
   private
-    def damage(attacker, attack, defender)
-      attack.attack - defender.armor
+    def damage(attacker, attack, attack_upgrade_level, defender, armor_upgrade_level)
+      attacker.attack(attack, attack_upgrade_level, defender, armor_upgrade_level)
     end
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -6,4 +6,18 @@ class Unit < ApplicationRecord
   def dps(attack, attack_upgrade_level)
     (attack[:attack] + attack[:attack_upgrade] * attack_upgrade_level) * attack[:attack_count] / attack[:cooldown]
   end
+
+  def attack(attack, attack_upgrade_level, defender, armor_upgrade_level)
+    defender_armor = defender[:armor] + defender[:armor_upgrade] * armor_upgrade_level
+    if defender[:sc_attributes].include? "Air"
+      if !attack.air
+        return
+      end
+    else
+      if !attack.ground
+        return
+      end
+    end
+    (attack[:attack] + attack[:attack_upgrade] * attack_upgrade_level - defender_armor) * attack[:attack_count]
+  end
 end
